162b9d4a3324074a3f30dcffacc85fa2f5a30f98
iamsubendu <dassubendudas23@gmail.com>
2020-11-19 01:03:46 +0530

    way to be completed totally

--- movie.js
+++ movie.js
@@ -109,3 +109,101 @@ function TopRatedMovies() {
 }
 TopRatedMovies() ;
 
+	const buttonElement = document.querySelector("#search");
+    const inputElement = document.querySelector("#inputValue");
+
+buttonElement.onclick = function(event) {
+  event.preventDefault();
+  const value = inputElement.value;
+  console.log("Value:",value);
+  searchMovie(value);
+  }
+function searchMovie(value) {
+	var url = `https://api.themoviedb.org/3/search/movie?api_key=9a6434ee9cfe93901b99601cc5900981&query=` + value;
+	const imgURL = "https://image.tmdb.org/t/p/w500";
+	fetch(url)
+	.then(res =>  { 
+		if(!res.ok) {
+			throw Error("Error");
+		}
+		return res.json()
+	})
+	.then(data => {
+		console.log(data);
+		//because we wanna map over movie in fetch
+
+		const html = data.results.map(movie =>  {
+			    if(movie.poster_path  !=  null) {
+			    	return `
+                    <div class="slider-box">
+                      <div class= "img-box">
+                        <img src= "${imgURL + movie.poster_path}" >
+                      </div>
+                      <span class="details">
+                        <p> ${movie.title} </p>
+			            <p> ${movie.release_date} </p>
+			            <p> ${movie.vote_average} </p>
+                      </span>
+			         </div>
+                  </div>`;
+			    }
+			
+		}).join('')
+		;
+		console.log(html);
+		document.querySelector("#movies-searchable").insertAdjacentHTML("afterbegin",html);
+	})
+	.catch(err => {
+		console.log(err);
+	})
+
+}
+
+
+buttonElement.onclick = function(event) {
+  event.preventDefault();
+  const value = inputElement.value;
+  console.log("Value:",value);
+  searchMovie(value);
+  }
+function searchMovie(value) {
+	var url = `https://api.themoviedb.org/3/search/movie?api_key=9a6434ee9cfe93901b99601cc5900981&query=` + value;
+	const imgURL = "https://image.tmdb.org/t/p/w500";
+	fetch(url)
+	.then(res =>  { 
+		if(!res.ok) {
+			throw Error("Error");
+		}
+		return res.json()
+	})
+	.then(data => {
+		console.log(data);
+		//because we wanna map over movie in fetch
+
+		const html = data.results.map(movie =>  {
+			    if(movie.poster_path  !=  null) {
+			    	return `
+                    <div class="slider-box">
+                      <div class= "img-box">
+                        <img src= "${imgURL + movie.poster_path}" >
+                      </div>
+                      <span class="details">
+                        <p> ${movie.title} </p>
+			                  <p> ${movie.release_date} </p>
+			                  <p> ${movie.vote_average} </p>
+                      </span>
+			         </div>
+                  </div>`;
+			    }
+			
+		}).join('')
+		;
+		console.log(html);
+		document.querySelector("#movies-searchable").insertAdjacentHTML("afterbegin",html);
+	})
+	.catch(err => {
+		console.log(err);
+	})
+
+}
+
