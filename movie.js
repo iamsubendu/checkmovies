de74ee79380134b419d4b62af9888f01b739e4e2
iamsubendu <dassubendudas23@gmail.com>
2020-11-20 02:25:14 +0530

    api part and responsivenes is added as well as onclick function is implemented

--- movie.js
+++ movie.js
@@ -18,10 +18,13 @@ function UpComingMovies() {
                       <div class= "img-box">
                         <img src= "${imgURL + movie.poster_path}" class="model">
                       </div>
-                      <span class="details">
-                        <p> ${movie.title} </p>
+											<span class="details">
+											<b>
+                        <p class="header"> ${movie.title} </p>
 			            <p> ${movie.release_date} </p>
-			            <p> ${movie.vote_average} </p>
+									<p class="rating">&starf; ${movie.vote_average} </p>
+									<button class="popup" id="popup" onclick="togglePopup(${movie.id})">More Details</button>
+									</b>
                       </span>
 			         </div>
                   </div>`;
@@ -54,10 +57,13 @@ function PopularMovies() {
                       <div class= "img-box">
                         <img src= "${imgURL + movie.poster_path}" class="model">
                       </div>
-                      <span class="details">
+											<span class="details">
+											<b>
                         <p> ${movie.title} </p>
 			            <p> ${movie.release_date} </p>
-			            <p> ${movie.vote_average} </p>
+									<p class="rating">&starf; ${movie.vote_average} </p>
+									<button class="popup" id="popup" onclick="togglePopup(${movie.id})">More Details</button>
+									</b>
                       </span>
 			         </div>
                   </div>`;
@@ -90,10 +96,13 @@ function TopRatedMovies() {
                       <div class= "img-box">
                         <img src= "${imgURL + movie.poster_path}" class="model">
                       </div>
-                      <span class="details">
+											<span class="details">
+											<b>
                         <p> ${movie.title} </p>
 			            <p> ${movie.release_date} </p>
-			            <p> ${movie.vote_average} </p>
+									<p class="rating">&starf; ${movie.vote_average} </p>
+									<button class="popup" id="popup" onclick="togglePopup(${movie.id})">More Details</button>
+									</b>
                       </span>
 			         </div>
                   </div>`;
@@ -139,10 +148,13 @@ function searchMovie(value) {
                       <div class= "img-box">
                         <img src= "${imgURL + movie.poster_path}" >
                       </div>
-                      <span class="details">
+											<span class="details">
+											<b>
                         <p> ${movie.title} </p>
 			            <p> ${movie.release_date} </p>
-			            <p> ${movie.vote_average} </p>
+									<p class="rating">&starf; ${movie.vote_average} </p>
+									<button class="popup"  id="popup" onclick="togglePopup(${movie.id})">More Details</button>
+									</b>
                       </span>
 			         </div>
                   </div>`;
@@ -160,14 +172,10 @@ function searchMovie(value) {
 }
 
 
-buttonElement.onclick = function(event) {
-  event.preventDefault();
-  const value = inputElement.value;
-  console.log("Value:",value);
-  searchMovie(value);
-  }
-function searchMovie(value) {
-	var url = `https://api.themoviedb.org/3/search/movie?api_key=9a6434ee9cfe93901b99601cc5900981&query=` + value;
+
+function togglePopup(id) {
+	console.log(id);
+		var url = `https://api.themoviedb.org/3/movie/${id}?api_key=9a6434ee9cfe93901b99601cc5900981`;
 	const imgURL = "https://image.tmdb.org/t/p/w500";
 	fetch(url)
 	.then(res =>  { 
@@ -180,30 +188,22 @@ function searchMovie(value) {
 		console.log(data);
 		//because we wanna map over movie in fetch
 
-		const html = data.results.map(movie =>  {
-			    if(movie.poster_path  !=  null) {
-			    	return `
-                    <div class="slider-box">
-                      <div class= "img-box">
-                        <img src= "${imgURL + movie.poster_path}" >
-                      </div>
-                      <span class="details">
-                        <p> ${movie.title} </p>
-			                  <p> ${movie.release_date} </p>
-			                  <p> ${movie.vote_average} </p>
-                      </span>
-			         </div>
-                  </div>`;
-			    }
-			
-		}).join('')
-		;
-		console.log(html);
-		document.querySelector("#movies-searchable").insertAdjacentHTML("afterbegin",html);
+		document.getElementById("popup").innerHTML=  `
+                  <div class="popup" id="popup-1">
+                    <div class="overlay"></div>
+                    <div class="content">
+                      <div class="close-btn" id="content-close" onclick="togglePopup(${id})">&times;</div>
+											<h1>${data.title}</h1>
+											<img src= "${imgURL + data.poster_path}" >
+											<p>${data.overview}</p>
+											<p class="rating">&starf; ${data.vote_average} </p>
+											<p> ${data.release_date} </p>
+                    </div>
+                  </div>
+									`; 
+									  document.getElementById("popup-1").classList.toggle("active");
 	})
 	.catch(err => {
 		console.log(err);
 	})
-
 }
-
